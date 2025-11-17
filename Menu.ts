0d1a2865f1134145d1aa7abcb91c2590f212b5ca
Bowie <cleiton-andrade@outlook.com>
2025-11-17 15:07:23 -0300

    Add Controller

--- Menu.ts
+++ Menu.ts
@@ -1,8 +1,14 @@
 import readlinesync = require("readline-sync");
+import { ProdutoController } from "./src/Controller/ProdutoController";
+import { Calcados } from "./src/model/Calcados";
 
 export function main() {
 
-    let opcao: number;
+    let produtos: ProdutoController = new ProdutoController()
+
+    let opcao, numero, tipo, valor, tamanho: number
+    let marca: string
+    const tiposCalcados = ['sapato', 'tenis']
 
     while (true) {
 
@@ -13,8 +19,8 @@ export function main() {
         console.log("                                                     ");
         console.log("            1 - Cadastrar produto                    ");
         console.log("            2 - Procurar produto                     ");
-        console.log("            3 - Atualizar produto                    ");
-        console.log("            4 - Excluir produto                      ");
+        console.log("            3 - Excluir produto                      ");
+        console.log("            4 - Listar todos os Produtos             ");
         console.log("            5 - Sair                                 ");
         console.log("_____________________________________________________");
         console.log("                                                     ");
@@ -30,29 +36,60 @@ export function main() {
 
         switch (opcao) {
             case 1:
-                console.log("\n\nCadastrar produto\n\n");
+                console.log("\n\nCriar Conta\n\n");
+                                
+                console.log("Digite o nome da marca do produto: ");
+                marca = readlinesync.question();
 
-                break;
+                console.log("Digite o tipo do calçado: ");
+                tipo = readlinesync.keyInSelect(tiposCalcados, '', { cancel: false }) + 1;
+
+                console.log("Digite o tamanho do calçado: ");
+                tamanho = readlinesync.questionInt();
+
+                console.log("Digite o valor do produto (R$): ");
+                valor = readlinesync.questionFloat();
+
+            produtos.cadastrar(
+                new Calcados(produtos.gerarNumero(), marca, valor , tamanho, tipo)
+                );
+
+            keyPress();
+            break;
+            
             case 2:
                 console.log("\n\nProcurar produto\n\n");
-
+                console.log("Digite o numero do produto: ")
+                numero = readlinesync.questionInt()
+                produtos.procurarPorNumero(numero)
+                
+                keyPress()
                 break;
+
             case 3:
-                console.log("\n\nAtualizar produto\n\n");
-
-                break;
-            case 4:
+                
                 console.log("\n\nExcluir produto\n\n");
 
+                console.log("Digite o numero do produto: ")
+                numero = readlinesync.questionInt()
+                produtos.deletar(numero)
+                
+                keyPress()
                 break;
-            case 5:
-                console.log("\n\nSair\n\n");
 
+            case 4:
+                console.log("\n\nListar todos os Produtos\n\n");
+
+
+                produtos.listarTodas()
+                
+                keyPress()
                 break;
 
             default:
                 console.log("\nOpção Inválida!\n");
-
+                
+                keyPress()
                 break;
         }
     }
@@ -63,3 +100,8 @@ export function sobre(): void {
     console.log("_____________________________________________________");
 }
 main();
+
+function keyPress(): void {
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+  }
--- src/repository/IProdutoRepository.ts
+++ src/repository/IProdutoRepository.ts
@@ -5,6 +5,5 @@ export interface IProdutoRepository {
     procurarPorNumero(numero: number): void;
     listarTodas(): void;
     cadastrar(Produto:Produto): void;
-    atualizar(Produto:Produto): void;
     deletar(numero: number): void;
 }
